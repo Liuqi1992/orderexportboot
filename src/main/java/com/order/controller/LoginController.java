d0b9e3b472f69a789bd18bc60ccb33889ea37159
liuqi1992 <1263793981@qq.com>
2022-04-10 21:58:02 +0800

    页面框架

--- src/main/java/com/order/controller/LoginController.java
+++ src/main/java/com/order/controller/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,7 +12,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LoginController {
 	
 	@RequestMapping(value="/user/login",method=RequestMethod.GET)
-	public String welcome(){
+	public String login(){
 		return "/vm/user/login";
 	}
+
+	@RequestMapping(value = "/user/logining", method=RequestMethod.POST)
+    @ResponseBody
+	public String logining(@RequestParam String username, @RequestParam String password) {
+	    return "aa";
+    }
+
+    @RequestMapping(value = "/user/loginsys", method=RequestMethod.GET)
+    public String loginsys() {
+        return "/vm/system/login";
+    }
 }
